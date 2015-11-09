0e976b636a212dd40118bd316a01afc1cb3f4def
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2015-11-09 11:48:12 +0100

    Increment the version number to prepare the next dev release

--- src/vortex/__init__.py
+++ src/vortex/__init__.py
@@ -23,7 +23,7 @@ of the very high level interface defined in the :mod:`vortex.toolbox` module is
 strongly advised.
 """
 
-__version__ = '0.9.22'
+__version__ = '0.9.23'
 __prompt__  = 'Vortex v-' + __version__ + ':'
 
 __all__ = []
