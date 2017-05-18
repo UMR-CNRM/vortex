c5e5ae1235b2448926674a8a40ab096ffeea49f4
Matthieu <matthieu.vernay@meteo.fr>
2017-05-18 12:18:19 +0000

    Typos

--- src/gco/tools/uenv.py
+++ src/gco/tools/uenv.py
@@ -91,7 +91,7 @@ def as_rawstr(cycle, scheme=None, netloc=None):
 
 
 def cycles():
-    """Return curretnly defined cycles."""
+    """Return currently defined cycles."""
     p = handler()
     grep = p.grep(_DATASTORE_KIND, dict())
     return [k.cycle for k in grep.keys()]
