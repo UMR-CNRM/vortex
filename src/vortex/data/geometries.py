745d4ad90649382725bd7910b4571740f0bee5c6
Matthieu <matthieu.vernay@meteo.fr>
2017-05-22 09:49:15 +0000

    nmassif is not a mandatory attribute of a massif geometry.

--- src/vortex/data/geometries.py
+++ src/vortex/data/geometries.py
@@ -339,11 +339,7 @@ class MassifGeometry(UnstructuredGeometry):
     def __init__(self, *args, **kw):
         super(MassifGeometry, self).__init__(**kw)
         self.kind = 'massif'
-
-    def _check_attributes(self):
-        super(MassifGeometry, self)._check_attributes()
-        if self.nmassif is None:
-            raise AttributeError("Some mandatory arguments are missing")
+        self.area = self.tag
 
     def doc_export(self):
         """Relevant informations to print in the documentation."""
