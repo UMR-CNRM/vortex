11ea7dbd4981d0454b0a084c12e0e9d815ba204c
Matthieu Vernay <matthieu.vernay@meteo.fr>
2017-08-04 10:34:23 +0200

    Fix namelist selection

--- src/cen/data/consts.py
+++ src/cen/data/consts.py
@@ -60,9 +60,6 @@ class NamelistSurfex(Namelist):
     def realkind(self):
         return 'surfex_namelist'
        
-    def realkind(self):
-        return 'surfex_namelist'
-       
     def contents_handler(self,**kw):
         self.clscontents(self.date)
         
