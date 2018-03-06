0528bd36942f7303d21303a3e0ae4a2e776bab70
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2018-03-06 15:40:58 +0100

    Bugfixes for Python3 and Sphinx documentation

--- tests/tests_bronx/test_doctests.py
+++ tests/tests_bronx/test_doctests.py
@@ -14,7 +14,7 @@ class utDocTests(unittest.TestCase):
     def assert_doctests(self, module, **kwargs):
         rc = doctest.testmod(module, **kwargs)
         self.assertEqual(rc[0], 0,  # The error count should be 0
-                         'Doctests errors {:s} for {!r}'.format(rc, module))
+                         'Doctests errors {!s} for {!r}'.format(rc, module))
 
     def test_doctests(self):
         self.assert_doctests(namelist)
