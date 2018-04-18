60cefd9f8532f4c4d7423faa0597212a6a05e28e
Gaëlle Rigoudy <gaelle.rigoudy@meteo.fr>
2018-04-18 10:04:05 +0200

    Change the seed in mix_list.

--- src/vortex/util/helpers.py
+++ src/vortex/util/helpers.py
@@ -156,16 +156,37 @@ def merge_contents(*kargs):
 
 def mix_list(list_elements, date = None, member = None):
     """Mix a list using a determined seed, if member and/or date are present."""
-
-    if member is None:
-        if date is None:
+    logger.info("The member is %s.", member)
+    logger.info("The date is %s.", date)
+    # Correct the values of member and date to avoid problems in the creation of the seed
+    used_date = None
+    if date is not None:
+        used_date = int(date)
+        if used_date == 0:
+            used_date = 1e10-1
+    used_member = None
+    if member is not None:
+        used_member = int(member)
+        if used_member == 0:
+            used_member = 1e4-1
+        used_member *= 1e6
+    if used_member is None:
+        if used_date is None:
             seed = random.random()
         else:
-            seed = 1./int(date)
+            seed = used_date/1e10
     else:
-        if date is None:
-            seed = 1./member
+        if used_date is None:
+            seed = used_member/1e10
         else:
-            seed = 1.*member/int(date)
-    seed = None
-    return random.shuffle(list_elements.sort(), lambda: seed)
+            if used_member < used_date:
+                seed = used_member*1./used_date
+            else:
+                seed = used_date*1./used_member
+    logger.info("The seed is %s.", seed)
+    logger.info("The list of elements is %s.", " ".join([str(x) for x in list_elements]))
+    result_list_elements = list_elements
+    result_list_elements.sort()
+    random.shuffle(result_list_elements, lambda: seed)
+    logger.info("The mixed list of elements is %s.", " ".join([str(x) for x in result_list_elements]))
+    return result_list_elements
