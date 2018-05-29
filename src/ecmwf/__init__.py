da133a3bc31de9bede6660b069e2e7d6fba1f437
Gaëlle Rigoudy <gaelle.rigoudy@meteo.fr>
2018-05-29 09:02:54 +0200

    Add ECtrans and ECfs interfaces and tests.
    (cherry picked from commit ae340ad)

---
+++ src/ecmwf/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+The ECMWF VORTEX extension package.
+"""
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+# Recursive inclusion of packages with potential FootprintBase classes
+from . import tools
+
+#: No automatic export
+__all__ = []
+
+__tocinfoline__ = 'The ECMWF VORTEX extension'
