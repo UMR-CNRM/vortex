f5e4861a166da123d5633e71139077f04c889db9
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2018-12-11 17:20:53 +0100

    Fix CCA and CCB config

--- site/ecmwf_system/systems.py
+++ site/ecmwf_system/systems.py
@@ -73,6 +73,6 @@ class CCB(ECMWFCrayXC):
             ),
         ),
         only = dict(
-            hostname = footprints.FPRegex(r'cca(?:(?:-login|ppn)?\d+|-batch)(?:\.|$)')
+            hostname = footprints.FPRegex(r'ccb(?:(?:-login|ppn)?\d+|-batch)(?:\.|$)')
         ),
     )
