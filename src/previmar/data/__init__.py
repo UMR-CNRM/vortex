a68550140978cb91bc6e93143e7ca3e9604e17ec
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2020-01-15 11:44:55 +0100

    Correct rebase errors

--- src/previmar/data/__init__.py
+++ src/previmar/data/__init__.py
@@ -7,7 +7,7 @@ Specific PreviMar data resources.
 
 from __future__ import print_function, absolute_import, unicode_literals, division
 
-from . import consts, executables, resources, contents, namelists, stores
+from . import consts, executables, resources, contents, namelists
 
 #: No automatic export
 __all__ = []
