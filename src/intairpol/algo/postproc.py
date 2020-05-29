5b6499874ba4db4b4c29a42477dabc34857b4065
Laurent Goulard <laurent.goulard@meteo.fr>
2020-05-29 08:38:02 +0200

    Modify Mocage post-processing Algo Component
    Allow more than one namelist (one per domain)
    Add an option to compule daily statistics

--- src/intairpol/algo/postproc.py
+++ src/intairpol/algo/postproc.py
@@ -6,8 +6,13 @@ AlgoComponents for MOCAGE post-processing.
 """
 
 from __future__ import absolute_import, print_function, division, unicode_literals
-
 import re
+from pprint import pprint
+from collections import defaultdict
+
+import numpy as np
+from netCDF4 import Dataset, MFDataset, num2date, date2num
+from datetime import datetime, timedelta
 
 from bronx.datagrip.namelist import NamelistBlock
 from bronx.fancies import loggers
@@ -43,6 +48,11 @@ class PPCamsBDAP(BlindRun):
                     optional = True,
                     default  = 'namelistgrib2.nam',
                 ),
+                daily_values = dict(
+                    type = bool,
+                    optional = True,
+                    default = False,
+                ),
             )
         )
     ]
@@ -51,76 +61,145 @@ class PPCamsBDAP(BlindRun):
     def realkind(self):
         return 'ppcamsbdap'
 
+    def compute_daily_values(self, rh):
+        """ compute daily statistics on forecast outputs."""
+        if rh.resource.geometry.area not in self.stats_files:
+            self.stats_files[rh.resource.geometry.area] = list()
+            if rh.resource.term.hour % 24 == 0:
+                return 
+        self.stats_files[rh.resource.geometry.area].append(rh.container.localpath())
+        if rh.resource.term.hour % 24 != 0:
+            return
+        else:
+            # list of variables to process
+            species_list = ['O_x', 'NO_2', 'NO', 'CO', 'SO_2', 'PM10', 'PM2.5']
+            # Open all the files simultaneously for the day considered
+            all_files = MFDataset(self.stats_files[rh.resource.geometry.area])
+            # Read a single example file
+            example_file = Dataset(self.stats_files[rh.resource.geometry.area][0])
+            # Open the output files
+            output_file = Dataset('ppstats.mocage-daily.{}+{:04d}.netcdf'.format(
+                                      rh.resource.geometry.area,
+                                      rh.resource.term.hour),
+                                  'w')
+            # Copy global attributes
+            output_file.setncatts({k: example_file.getncattr(k) for k in example_file.ncattrs()})
+            # Copy the dimensions
+            for dim_name, dim_values in example_file.dimensions.iteritems():
+                output_file.createDimension(dim_name, 
+                                            len(dim_values) if not dim_values.isunlimited() 
+                                                            else None)
+            # Process each variable
+            for var_name, var_values in example_file.variables.iteritems():
+                var_dim = len(var_values.dimensions)
+                # 1D and 2D variables are just copied in the output files
+                if var_dim < 3:
+                    output_var = output_file.createVariable(var_name,
+                                                            var_values.datatype, 
+                                                            var_values.dimensions)
+                    output_var.setncatts({k: var_values.getncattr(k) for k in var_values.ncattrs()})
+                    output_var[:] = var_values[:]
+                # Other variables are processed
+                elif var_name in species_list:
+                    # Read the whole data
+                    data = np.array(all_files.variables[var_name][:])
+                    # Daily mean in each grid-point
+                    output_var = output_file.createVariable(var_name + '_mean', var_values.datatype,
+                                                              var_values.dimensions)
+                    output_var.setncatts({k: var_values.getncattr(k) for k in var_values.ncattrs()})
+                    output_var[:] = np.mean(data, axis=0, keepdims=True)
+                    # Daily maximum in each grid-point
+                    output_var = output_file.createVariable(var_name + '_max', var_values.datatype,
+                                                              var_values.dimensions)
+                    output_var.setncatts({k: var_values.getncattr(k) for k in var_values.ncattrs()})
+                    output_var[:] = np.max(data, axis=0, keepdims=True)
+            # Close the files
+            all_files.close()
+            example_file.close()
+            output_file.close()
+            self.stats_files[rh.resource.geometry.area] = list()
+
     def execute(self, rh, opts):
         """Standard execution."""
-
         sh = self.system
-
+        if self.daily_values:
+            self.stats_files = dict()
         # Namelist
-        namrh = self.context.sequence.effective_inputs(
+        namelists = self.context.sequence.effective_inputs(
             role='Namelist',
             kind='namelist',)
-        if len(namrh) != 1:
-            logger.critical('There must be exactly one namelist for maccraq execution. Stop.')
-            raise ValueError('There must be exactly one namelist for maccraq execution. Stop.')
-
-        namrh = namrh[0].rh
-        if not namrh.container.is_virtual() and sh.path.basename(namrh.container.localpath()) == self.namelist_name:
-            logger.critical('The namelist cannot be named "%s".', self.namelist_name)
-            raise ValueError()
-        refblock = NamelistBlock(name='MACCRAQ_IN')
-        refblock.update(namrh.contents['MACCRAQ_IN'])
-
+        template_multi = r'namelistgrib2_(\w+)\.nam'
+        if len(namelists) ==0:
+            message = 'there must be at least one namelist'
+            logger.critical(message)
+            raise ValueError(message)
+        elif len(namelists) == 1:
+            namrh = namelists[0].rh
+            if not namrh.container.is_virtual() and sh.path.basename(namrh.container.localpath()) == self.namelist_name:
+                logger.critical('The namelist cannot be named "%s".', self.namelist_name)
+                raise ValueError()
+            geometries = (None, )
+        else:
+            geometries = map(lambda n:re.match(template_multi,n.rh.container.localpath()), namelists)
+            if not all(geometries):
+                message = 'If there is more than 1 Namelist, they must be named "namelistgrib2_DOM.nam" .  STOP'
+                logger.critical(message)
+                raise ValueError(message)
+            else:
+                geometries = [ m.group(1) for m in geometries ]
+        # loop on domains to construct the reference Namelists  
+        if None in geometries:
+            maccraq_blocks = defaultdict(lambda : maccraq_blocks[None])
+            contents = defaultdict(lambda : contents[None])
+        else:
+            maccraq_blocks = dict()
+            contents = dict()
+        for namelist, geometry in zip(namelists, geometries):
+            print('geometry : {},namelist : {}'.format(geometry, namelist))
+            contents[geometry] = namelist.rh.contents
+            maccraq_blocks[geometry] = NamelistBlock(name='MACCRAQ_IN')
+            maccraq_blocks[geometry].update(namelist.rh.contents['MACCRAQ_IN'])
         # HM files from forecast
         hmrh = self.context.sequence.effective_inputs(
-            role='HMFiles',
-            kind='gridpoint')
+                   role='HMFiles',
+                   kind='gridpoint')
         # overwrite hmrh by the ascending sort of the hmrh list
         hmrh.sort(key=lambda s: s.rh.resource.term)
-
         for i in hmrh:
             r = i.rh
-
             # wait for the next HM netcdf file to be translated in grib2 format
             self.grab(i, comment='forecast outputs moved to grib2 format')
-
             sh.title('Loop on domain {0:s} and term {1:s}'.format(r.resource.geometry.area,
                                                                   r.resource.term.fmthm))
             actualdate = r.resource.date + r.resource.term
-
+            # optionaly compute daily statistics
+            if self.daily_values:
+                self.compute_daily_values(r)
             # Get a temporary namelist container
             newcontainer = footprints.proxy.container(filename=self.namelist_name, format='txt')
-
             # Substitute macros in namelist
-            myblock = namrh.contents['MACCRAQ_IN']
+            myblock = contents[r.resource.geometry.area]['MACCRAQ_IN']
             myblock.clear()
-            myblock.update(refblock)
+            myblock.update(maccraq_blocks[r.resource.geometry.area])
             myblock.addmacro('YYYY', actualdate.year)
             myblock.addmacro('MM', actualdate.month)
             myblock.addmacro('DD', actualdate.day)
             myblock.addmacro('HH', actualdate.hour)
             myblock.addmacro('YYYYMMJJBASE', int(r.resource.date.ymd))
             myblock.addmacro('ST', int(r.resource.term.hour))
-
-            namrh.contents.rewrite(newcontainer)
+            contents[r.resource.geometry.area].rewrite(newcontainer)
             newcontainer.cat()
-
             # Link in the forecast file
             self.system.softlink(r.container.localpath(), 'HMFILE.nc')
-
             # Execute
             super(PPCamsBDAP, self).execute(rh, opts)
-
             newcontainer.clear()
-
             actualname = 'MFM_' + actualdate.ymdh + '.grib2'
             if self.system.path.exists('MFM_V5-.grib2'):
                 sh.mv('MFM_V5-.grib2', actualname)
             if self.system.path.exists('MFM_V5+.grib2'):
                 sh.mv('MFM_V5+.grib2', actualname)
-
             sh.rmall('HMFILE.nc', 'HM_HYBRID.nc', 'HM.nc')
-
             # The grib2 output may be promised for BDAP transferts : put method applied to these outputs
             # put these outputs in the cache ; IGA will perform the following actions.
             expected = [x for x in self.promises
@@ -170,105 +249,3 @@ class MkStatsCams(Expresso):
             verbose='',
         )
 
-
-class PPprevairBDAP(Parallel):
-    """
-    Post-processing of mocage/prevair fc for BDAP.
-    """
-
-    _footprint = [
-        model,
-        dict(
-            info = 'Post-processing of mocage/prevair fc for BDAP',
-            attr = dict(
-                kind = dict(
-                    values   = ['ppprevairbdap'],
-                ),
-                model = dict(
-                    values   = ['mocage'],
-                ),
-                namelist_name = dict(
-                    info     = 'Namelist name for the binary',
-                    optional = True,
-                    default  = 'PREVIBASE_param.nam',
-                ),
-            )
-        )
-    ]
-
-    @property
-    def realkind(self):
-        return 'ppprevairbdap'
-
-    def _fix_nam_macro(self, rh, macro, value):
-        """Set a given namelist macro and issue a log message."""
-        rh.contents.setmacro(macro, value)
-        logger.info('Setup %s macro to %s in %s', macro, value, rh.container.actualpath())
-
-    def execute(self, rh, opts):
-        """Standard execution."""
-        sh = self.system
-
-        # Namelist
-        namrh = self.context.sequence.effective_inputs(
-            role='Namelist',
-            kind='namelist',)
-        if len(namrh) != 1:
-            logger.critical('There must be exactly one namelist for hmsplit_grib2 execution. Stop.')
-            raise ValueError('There must be exactly one namelist for hmsplit_grib2 execution. Stop.')
-
-        namrh = namrh[0].rh
-        if not namrh.container.is_virtual() and sh.path.basename(namrh.container.localpath()) == self.namelist_name:
-            logger.critical('The namelist cannot be named "%s".', self.namelist_name)
-            raise ValueError()
-
-        # save the parametrized namelist
-        sh.cp('PREVIBASE.nam', 'PREVIBASE_param.nam')
-
-        # HM files from forecast
-        hmrh = self.context.sequence.effective_inputs(
-            role='HMFiles',
-            kind='GridPoint')
-        logger.info('Number of HMFilesFA %d ', len(hmrh))
-
-        if len(hmrh) == 0:
-            logger.critical('There must be HM files to be opened by hmsplit_grib execution. Stop.')
-            raise ValueError('There must be HM files to be opened by hmsplit_grib2 execution. Stop.')
-
-        # overwrite hmrh by the ascending sort of the hmrh list
-        hmrh.sort(key=lambda s: s.rh.resource.term)
-
-        for i in hmrh:
-            r = i.rh
-
-            # wait for the next HM fa file to be translated in grib2 format
-            self.grab(i, comment='forecast outputs moved to grib2 format')
-
-            sh.title('Loop on domain {0:s} and term {1:s}'.format(r.resource.geometry.area,
-                                                                  r.resource.term.fmthm))
-            actualdate = r.resource.date + r.resource.term
-
-            # Get a new parametrized namelist
-            sh.cp('PREVIBASE_param.nam', 'PREVIBASE.nam', intent='in')
-            sh.cat('PREVIBASE.nam', output=False)
-
-            self._fix_nam_macro(namrh, 'YYYY', int(r.resource.date.year))
-            self._fix_nam_macro(namrh, 'MM', int(r.resource.date.month))
-            self._fix_nam_macro(namrh, 'DD', int(r.resource.date.day))
-            self._fix_nam_macro(namrh, 'HH', int(r.resource.term))
-            self._fix_nam_macro(namrh, 'DATEECH', int(actualdate.ymdh))
-
-            namrh.save()
-            namrh.container.cat()
-
-            # Execute
-            super(PPprevairBDAP, self).execute(rh, opts)
-
-            actualname = 'GRIB_BDAP_' + actualdate.ymdh
-            # The grib2 output may be promised for BDAP transferts : put method
-            # applied to these outputs. put these outputs in the cache ; IGA
-            # will perform the following actions.
-            expected = [x for x in self.promises
-                        if (re.match(actualname, x.rh.container.localpath()))]
-            for thispromise in expected:
-                thispromise.put(incache=True)
