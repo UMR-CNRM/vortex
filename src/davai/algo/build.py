fa65948998987007dcd8c26ecbd846abc9187c2d
Alexandre MARY <alexandre.mary@meteo.fr>
2020-07-24 17:47:43 +0200

    gmkpack deco mixin

--- src/davai/algo/build.py
+++ src/davai/algo/build.py
@@ -5,12 +5,14 @@ DAVAI sources build (branch export, compilation&link) AlgoComponents.
 """
 from __future__ import print_function, absolute_import, unicode_literals, division
 
+import footprints
 from footprints import FPList, FPDict
 from bronx.fancies import loggers
 
 from vortex.syntax import stdattrs
 from vortex.algo.components import (AlgoComponent, AlgoComponentDecoMixin,
                                     AlgoComponentError)
+from vortex.algo.components import algo_component_deco_mixin_autodoc
 from gco.tools import uenv, genv
 
 from .mixins import _CrashWitnessDecoMixin
@@ -22,10 +24,39 @@ __all__ = []
 logger = loggers.getLogger(__name__)
 
 
-class IA4H_gitref_to_Pack(AlgoComponent, _CrashWitnessDecoMixin):
-    """
-    Make a pack (gmkpack) with sources from a IA4H Git ref.
-    """
+@algo_component_deco_mixin_autodoc
+class GmkpackDecoMixin(AlgoComponentDecoMixin):
+    
+    _MIXIN_EXTRA_FOOTPRINTS = (footprints.Footprint(
+        info="Abstract mbdetect footprint",
+        attr=dict(
+            homepack = dict(
+                info = "Home directory for pack.",
+                optional = True,
+                default = None
+            ),
+            cleanpack = dict(
+                info = "Whether to cleanpack a pack before modifying it.",
+                type = bool,
+                optional = True,
+                default = True
+            )
+        )
+    ),)
+    
+    def _set_gmkpack(self, rh, opts):  # @UnusedVariable
+        gmk_installdir = self.target.config.get('gmkpack', 'gmkpack_installdir')
+        self.env['PATH'] = ':'.join([self.system.path.join(gmk_installdir, 'util'),
+                                     self.env['PATH']])
+        self.env['GMKROOT'] = self.system.path.join(gmk_installdir)
+        self.env['GMKTMP'] = self.system.getcwd() 
+    
+    _MIXIN_PREPARE_HOOKS = (_set_gmkpack, )
+
+
+class IA4H_gitref_to_Pack(AlgoComponent, GmkpackDecoMixin,
+                          _CrashWitnessDecoMixin):
+    """Make a pack (gmkpack) with sources from a IA4H Git ref."""
 
     _footprint = [
         dict(
@@ -51,21 +82,10 @@ class IA4H_gitref_to_Pack(AlgoComponent, _CrashWitnessDecoMixin):
                     optional = True,
                     default = False,
                 ),
-                cleanpack = dict(
-                    info = "Whether to cleanpack a preexisting pack.",
-                    type = bool,
-                    optional = True,
-                    default = True
-                    ),
                 rootpacks_dir = dict(
                     info = "Directory in which to find rootpacks.",
                     optional = True,
                     default = None,
-                ),
-                homepack = dict(
-                    info = "Home directory for pack.",
-                    optional = True,
-                    default = None
                 ),
                 other_pack_options = dict(
                     info = "Other options to be passed to gmkpack command.",
@@ -86,11 +106,6 @@ class IA4H_gitref_to_Pack(AlgoComponent, _CrashWitnessDecoMixin):
                                                           'libexec',
                                                           'git-core')
         # gmkpack
-        gmk_installdir = self.target.config.get('gmkpack', 'gmkpack_installdir')
-        self.env['PATH'] = ':'.join([self.system.path.join(gmk_installdir, 'util'),
-                                     self.env['PATH']])
-        self.env['GMKROOT'] = self.system.path.join(gmk_installdir)
-        self.env['GMKTMP'] = self.system.getcwd()
         if self.rootpacks_dir is None:
             rootpack = self.target.config.get('gmkpack', 'ROOTPACK')
             self.env['ROOTPACK'] = rootpack
@@ -110,12 +125,10 @@ class IA4H_gitref_to_Pack(AlgoComponent, _CrashWitnessDecoMixin):
                                    homepack=self.homepack,
                                    other_pack_options=self.other_pack_options)
 
-# TODO: Several projects/repos to pack
 
-class PackBuildExecutables(AlgoComponent, _CrashWitnessDecoMixin):
-    """
-    Compile sources and link executables within a pack (gmkpack).
-    """
+class PackBuildExecutables(AlgoComponent, GmkpackDecoMixin,
+                           _CrashWitnessDecoMixin):
+    """Compile sources and link executables within a pack (gmkpack)."""
 
     _footprint = [
         dict(
@@ -127,11 +140,6 @@ class PackBuildExecutables(AlgoComponent, _CrashWitnessDecoMixin):
                 packname = dict(
                     info = "The pack to be compiled.",
                 ),
-                homepack = dict(
-                    info = "Home directory for pack.",
-                    optional = True,
-                    default = None
-                ),
                 programs = dict(
                     info = "Programs to be built.",
                     optional = True,
@@ -139,12 +147,6 @@ class PackBuildExecutables(AlgoComponent, _CrashWitnessDecoMixin):
                 ),
                 regenerate_ics = dict(
                     info = "Whether to regenerate or not the ics_<program> scripts.",
-                    type = bool,
-                    optional = True,
-                    default = True
-                    ),
-                cleanpack = dict(
-                    info = "Whether to cleanpack or not before compiling.",
                     type = bool,
                     optional = True,
                     default = True
@@ -164,18 +166,6 @@ class PackBuildExecutables(AlgoComponent, _CrashWitnessDecoMixin):
             )
         )
     ]
-
-    def prepare(self, rh, opts):  # @UnusedVariable
-        from ia4h_scm.pygmkpack import USUAL_BINARIES
-        # gmkpack
-        gmk_installdir = self.target.config.get('gmkpack', 'gmkpack_installdir')
-        self.env['PATH'] = ':'.join([self.system.path.join(gmk_installdir, 'util'),
-                                     self.env['PATH']])
-        self.env['GMKROOT'] = self.system.path.join(gmk_installdir)
-        self.env['GMKTMP'] = self.system.getcwd()
-        # programs
-        if self.programs == '__usual__':
-            self._attributes['programs'] = USUAL_BINARIES
 
     def execute(self, rh, kw):  # @UnusedVariable
         from ia4h_scm.algos import pack_build_executables
