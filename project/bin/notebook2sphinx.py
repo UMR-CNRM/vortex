ef1b1c65a2d9ca3a5e7394b9ab6606f24f116ebf
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2020-09-28 16:04:44 +0200

    Bugfixes for nbconvert v6+ and sphinx v3+

--- project/bin/notebook2sphinx.py
+++ project/bin/notebook2sphinx.py
@@ -93,8 +93,12 @@ class DefaultExporter(object):
     def _ipynb_convert(self, a_file):
         """Actually convert the notebook."""
         myname = os.path.splitext(os.path.basename(a_file))[0]
-        exporter = RSTExporter(template_path=[_NBCONVERT_TEMPLATES, ],
-                               template_file=rst_tplfile)
+        if int(tplversion) >= 6:
+            exporter = RSTExporter(extra_template_basedirs=[_NBCONVERT_TEMPLATES, ],
+                                   template_name=rst_tplfile)
+        else:
+            exporter = RSTExporter(template_path=[_NBCONVERT_TEMPLATES, ],
+                                   template_file=rst_tplfile)
         rst, resources = exporter.from_filename(a_file,
                                                 resources=dict(unique_key=myname))
         logger.debug("%s exported. Name=%s, Outputs=%s",
