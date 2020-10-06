d5a21b2587cb340d1dfe97881d614ac5272bc875
CHAINE PREVI <mxpt001@belenoslogin4.belenoshpc.meteo.fr>
2020-10-06 16:25:40 +0000

    modif nom serveur mail

--- src/iga/tools/op.py
+++ src/iga/tools/op.py
@@ -164,7 +164,7 @@ class OpJobAssistantTest(JobAssistant):
         opd = kw.get('actual', dict())
         vortex.toolbox.defaults(
             jname=opd.get('op_jeeves', None),
-            smtpserver='mail.srv.belenoshpc.meteo.fr',
+            smtpserver='smtp.meteo.fr',
             sender='admin_prod_sc@meteo.fr',
         )
 
