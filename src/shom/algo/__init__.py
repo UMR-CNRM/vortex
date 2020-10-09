133f031d4b7f5238ed7791c2ea139d0dfc615e29
MORVAN MATHIEU <morvanm@belenoslogin0.belenoshpc.meteo.fr>
2020-10-09 14:00:49 +0000

    renommage repertoire sloop --> shom

---
+++ src/shom/algo/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:Utf-8 -*-
+
+"""
+Specific Sloop AlgoComponents.
+"""
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+import sloop
+
+from . import hycom3d
+
+#: No automatic export
+__all__ = []
