fba75527f4fc1fcf5b80f740818b3e738d3ff065
Stephane Raynaud <stephane.raynaud@shom.fr>
2020-11-09 14:01:05 +0100

    Removed noisy imports

--- src/shom/algo/__init__.py
+++ src/shom/algo/__init__.py
@@ -7,9 +7,6 @@ Specific Sloop AlgoComponents.
 
 from __future__ import print_function, absolute_import, unicode_literals, division
 
-import sloop
-
-from . import hycom3d
 
 #: No automatic export
 __all__ = []
