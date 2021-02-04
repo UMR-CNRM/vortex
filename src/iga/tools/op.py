79d6488721eaac20bb8296fe6f3e7376143de369
CHAINE PREVI <mxpt001@belenoslogin4.belenoshpc.meteo.fr>
2021-02-04 15:08:55 +0000

    adaptations hook bdpe pour mocage accident

--- src/iga/tools/op.py
+++ src/iga/tools/op.py
@@ -401,7 +401,7 @@ def oproute_hook_factory(kind, productid, sshhost, optfilter=None, soprano_targe
 
         if hasattr(rh.resource, 'geometry'):
             kwargs['domain'] = rh.resource.geometry.area
-        if hasattr(rh.resource, 'term'):
+        if hasattr(rh.resource, 'term') and not 'term' in kwargs:
             kwargs['term'] = rh.resource.term
             if kwargs['transmet']:
                 kwargs['transmet']['ECHEANCE'] = rh.resource.term.fmth
