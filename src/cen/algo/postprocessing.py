741ddb88be6b448aaf7f2628ab110871addc5680
radanovicss <sabine.radanovics@meteo.fr>
2021-06-22 17:35:34 +0200

    postprocessing algo with modifications suggested by lf.

--- src/cen/algo/postprocessing.py
+++ src/cen/algo/postprocessing.py
@@ -6,11 +6,15 @@ Algo Components for S2M post processing.
 """
 
 from __future__ import print_function, absolute_import, unicode_literals, division
+
 from bronx.fancies import loggers
-from bronx.stdtypes.date import Date
 from bronx.syntax.externalcode import ExternalCodeImportChecker
+from footprints.stdtypes import FPList
 
 from vortex.algo.components import AlgoComponent
+
+# from cen.syntax.stdattrs import cendateperiod
+
 logger = loggers.getLogger(__name__)
 
 echecker = ExternalCodeImportChecker('snowtools')
@@ -23,41 +27,35 @@ class S2m_ensemble_postprocessing(AlgoComponent):
     """
     S2M ensemble forecast postprocessing.
 
-    Current use : Get deciles of "fresh snow" (12-hourly and daily accumulation for the Bulletin 4 saisons)
+    Current use : Get ensemble deciles of "fresh snow" (12-hourly and daily accumulation for the Bulletin 4 saisons)
     """
-    _footprint = dict(
-        info = 'Algo component for post-processing of s2m ensemble simulations',
-        attr = dict(
-            kind = dict(
-                values = ['s2m_postproc']),
-            varnames = dict(
-                info = "Variable names to be post-processed",
-                type = list,
-                optional = False
-            ),
-            engine = dict(
-                optional    = True,
-                default     = 's2m',
-                values      = ['s2m']
-            ),
-            datebegin=dict(
-                info="Date in the namelist to run PREP.",
-                type=Date,
-            ),
-            dateend=dict(
-                info="Date in the namelist to stop OFFLINE.",
-                type=Date,
-                default=None
+    _footprint = [
+        dict(
+            info = 'Algo component for post-processing of s2m ensemble simulations',
+            attr = dict(
+                kind = dict(
+                    values = ['s2m_postproc']
+                ),
+                varnames = dict(
+                    info = "Variable names to be post-processed",
+                    type = FPList,
+                ),
+                engine = dict(
+                    optional    = True,
+                    default     = 's2m',
+                    values      = ['s2m']
+                ),
             ),
         ),
-    )
+    ]
 
     def execute(self, rh, opts):
         # get input resources
-        avail_forcasts = self.context.sequence.effective_inputs()  # role="Crocus Forecast"
+        avail_forecasts = self.context.sequence.effective_inputs(role="CrocusForecast")  # role="Crocus Forecast"
         # get list of file names
-        listforcing = [am.rh.container.filename for am in avail_forcasts]
+        listforcing = [am.rh.container.filename for am in avail_forecasts]
         # init ensemble postprocessing object
-        ens = EnsemblePostproc(Ensemble(), self.varnames, listforcing, self.datebegin, self.dateend)
+        ens = EnsemblePostproc(Ensemble(), self.varnames, listforcing, avail_forecasts[0].rh.resource.datebegin,
+                               avail_forecasts[0].rh.resource.dateend)
         # do postprocessing
         ens.postprocess()
