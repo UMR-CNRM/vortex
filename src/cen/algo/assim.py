94f1842ec074c4498caf73e16f69537a7755149e
Matthieu Lafaysse <matthieu.lafaysse@meteo.fr>
2022-07-28 18:21:50 +0200

    remove workSODA directory (not necessary complexity) + add preprocessing of namelist to adjust number of members

--- src/cen/algo/assim.py
+++ src/cen/algo/assim.py
@@ -8,10 +8,16 @@ from __future__ import print_function, absolute_import, unicode_literals, divisi
 
 from bronx.fancies import loggers
 from bronx.stdtypes.date import Date
+from bronx.syntax.externalcode import ExternalCodeImportChecker
+
 import footprints
-from vortex.algo.components import Parallel
+from vortex.algo.components import Parallel, AlgoComponent
 
 logger = loggers.getLogger(__name__)
+
+echecker = ExternalCodeImportChecker('snowtools')
+with echecker:
+    from snowtools.tools.update_namelist import update_namelist_object_nmembers
 
 class SodaWorker(Parallel):
     """
@@ -51,9 +57,7 @@ class SodaWorker(Parallel):
     def prepare(self, rh, opts):
         super(SodaWorker, self).prepare(rh, opts)
 
-        self.mbdirs = ['../mb{0:04d}'.format(m + 1) for m in range(len(self.members))]
-
-        self.system.cd('workSODA')
+        self.mbdirs = ['mb{0:04d}'.format(m + 1) for m in range(len(self.members))]
 
         # symbolic links for each prep from each member dir to the soda dir
         jj = 0
@@ -63,14 +67,6 @@ class SodaWorker(Parallel):
             jj += 1
         # symbolic link from a virtual PREP.nc to the first member (for SODA date-reading reasons)
         self.system.symlink(self.mbdirs[0] + '/PREP_' + self.dateassim.ymdh + '.nc', 'PREP.nc')
-
-        # the following should be done only once on the first soda day
-        if not self.system.path.islink('PGD.nc'):
-            self.system.symlink('../PGD.nc', 'PGD.nc')
-            self.system.symlink('../OPTIONS.nam', 'OPTIONS.nam')
-            self.system.symlink('../ecoclimapI_covers_param.bin', 'ecoclimapI_covers_param.bin')
-            self.system.symlink('../ecoclimapII_eu_covers_param.bin', 'ecoclimapII_eu_covers_param.bin')
-            self.system.symlink('../drdt_bst_fit_60.nc', 'drdt_bst_fit_60.nc')
 
     def execute(self, rh, opts):
         # run SODA
@@ -105,5 +101,44 @@ class SodaWorker(Parallel):
         else:
             print('no part file')
 
-        self.system.cd('..')
+@echecker.disabled_if_unavailable
+class Soda_PreProcess(AlgoComponent):
+    """Prepare SODA namelist according to configuration file"""
 
+    _footprint = dict(
+        attr = dict(
+            kind = dict(
+                values = ['soda_preprocess']),
+            engine = dict(
+                optional     = True,
+                default   = 'algo'
+            ),
+            members=dict(
+                info="The members that will be processed",
+                type=footprints.FPList,
+            ),
+        )
+    )
+
+    def find_namelists(self, opts=None):
+        """Find any namelists candidates in actual context inputs."""
+        namcandidates = [x.rh for x in self.context.sequence.effective_inputs(kind='namelist')]
+        self.system.subtitle('Namelist candidates')
+        for nam in namcandidates:
+            nam.quickview()
+
+        return namcandidates
+
+    def execute(self, rh, opts):
+
+        # Modification of the namelist
+        for namelist in self.find_namelists():
+            # Update the contents of the namelist (number of members)
+            # Location taken in the FORCING file.
+            newcontent = update_namelist_object_nmembers(
+                namelist.contents,
+                nmembers = len(self.members)
+            )
+            newnam = footprints.proxy.container(filename=namelist.container.basename)
+            newcontent.rewrite(newnam)
+            newnam.close()
