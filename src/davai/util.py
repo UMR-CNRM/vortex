65db3e929dd7fa45da15dcd7021b388e129c0eda
MARY Alexandre <alexandre.mary@meteo.fr>
2022-07-28 16:18:23 +0000

    Add token from test to the Ciboulai post

--- src/davai/util.py
+++ src/davai/util.py
@@ -14,6 +14,7 @@ import re
 import tarfile
 import tempfile
 import json
+import os
 
 from bronx.fancies import loggers
 
@@ -115,10 +116,18 @@ def send_task_to_DAVAI_server(davai_server_post_url, xpid, jsonData, kind,
 
     Additional kwargs are passed to requests.post()
     """
+    # token
+    token = os.environ.get('CIBOULAI_TOKEN', '')
+    if token == '':
+        logger.error("Environment variable CIBOULAI_TOKEN must be set in order to send data to Ciboulai. " +
+                     "Please contact your Davai admin to get a valid token.")
+        if fatal:
+            raise ValueError("Invalid CIBOULAI_TOKEN")
     # data to be sent to api
     data = {'jsonData': jsonData,
             'xpid': xpid,
-            'type': kind}
+            'type': kind,
+            'token':token}
     # sending post request and saving response as response object
     try:
         rc, status, headers, rdata = http_post_data(url=davai_server_post_url, data=data, **kwargs)
