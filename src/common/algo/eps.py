8008941ce8a4f8af2f66f2d897fda92c04f1e412
Pascal Lamboley <pascal.lamboley@meteo.fr>
2022-08-11 17:34:59 +0200

    Simplify the way to get the available terms
    -- toomany

--- src/common/algo/eps.py
+++ src/common/algo/eps.py
@@ -15,6 +15,7 @@ import footprints
 import six
 from bronx.compat.itertools import pairwise
 from bronx.fancies import loggers
+from bronx.stdtypes.date import Time
 from common.tools.drhook import DrHookDecoMixin
 from vortex.algo.components import BlindRun
 from vortex.layout.dataflow import intent
@@ -476,25 +477,11 @@ class Clustering(BlindRun, EcGribDecoMixin):
 
         # If no population file is here, just do a sort on the file list,
         # otherwise use the population list
-
-        # PLPL doit aller dans la branche "else" du "if avail_json"
-        #      mais mis ici temporairement pour tester le "cas not avail_json"
-        terms = sorted({grib.rh.resource.term for grib in grib_sections})
-        delta = {last - first for first, last in pairwise(terms)}
-        if len(delta) != 1:
-            logger.error('Terms are not evenly spaced. What should we do ?')
-            logger.error('Terms=' + str(terms) + 'delta=' + str(delta))
-        clustdeb = terms[0].hour
-        clustfin = terms[-1].hour
-        cluststep = delta.pop().hour
-        print('PLPL not avail_json: {}/{} by {}'.format(clustdeb, clustfin, cluststep))
-        # PLPL fin du test 'if avail_json'
-
         if avail_json:
             population = avail_json[0].rh.contents.data['population']
             self.nbmembers = len(population)
             file_list = list()
-            terms_info = list()
+            terms_set = set()
             for elt in population:
                 sublist_ids = list()
                 for (i, grib) in enumerate(grib_sections):
@@ -505,28 +492,27 @@ class Clustering(BlindRun, EcGribDecoMixin):
                 # Stack the gribs in file_list
                 file_list.extend(sorted([six.text_type(grib_sections[i].rh.container.localpath())
                                          for i in sublist_ids]))
-                terms_info.append((elt, sorted([grib_sections[i].rh.resource.term for i in sublist_ids])))
+                terms_set.update([grib_sections[i].rh.resource.term for i in sublist_ids])
                 for i in reversed(sublist_ids):
                     del grib_sections[i]
-
-            term_minis = {min(terms) for elt, terms in terms_info}
-            term_maxis = {max(terms) for elt, terms in terms_info}
-            term_diffs = {last - first
-                          for first, last in pairwise(sorted(terms))
-                          for elt, terms in terms_info}
-            if len(term_minis) != 1 or len(term_maxis) != 1 or len(term_diffs) != 1:
-                logger.error('Terms are consistent. What should we do ?')
-                logger.error('\n\n'.join([
-                    str(elt) + ' terms=' + str(terms) for elt, terms in terms_info
-                ]))
-            clustdeb = term_minis.pop().hour
-            clustfin = term_maxis.pop().hour
-            cluststep = term_diffs.pop().hour
-            print('PLPL case avail_json: {}/{} by {}'.format(clustdeb, clustfin, cluststep))
-
         else:
             file_list = sorted([six.text_type(grib.rh.container.localpath())
                                 for grib in grib_sections])
+            terms_set = {grib.rh.resource.term for grib in grib_sections}
+
+        # determine what terms are available to the clustering algorithm
+        terms = sorted(terms_set - {Time(0)})
+        delta = {last - first for first, last in pairwise(terms)}
+        if len(delta) == 1:
+            cluststep = delta.pop().hour
+        else:
+            cluststep = -999
+            logger.error('Terms are not evenly spaced. What should we do ?')
+            logger.error('Terms=' + str(terms) + 'delta=' + str(delta))
+            logger.error('Continuing with little hope and cluststep = %d', cluststep)
+        clustdeb = terms[0].hour
+        clustfin = terms[-1].hour
+        logger.info('clustering deb=%d fin=%d step=%d', clustdeb, clustfin, cluststep)
 
         # Deal with xGribs
         file_list_cat = [f + '.concatenated' for f in file_list]
