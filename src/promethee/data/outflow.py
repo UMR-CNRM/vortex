096065b02b84ce06acdef85ab7f8a6864343265c
lepapeb <brice.lepape@meteo.fr>
2022-09-09 09:41:03 +0000

    feat: adding a custom PrometheeMask resource containing hgeometry info

--- src/promethee/data/outflow.py
+++ src/promethee/data/outflow.py
@@ -8,8 +8,10 @@ from __future__ import print_function, absolute_import, unicode_literals, divisi
 
 from vortex.data.resources import Resource
 from vortex.data.outflow import StaticResource
-from promethee.syntax.stdattrs import promid_deco, version_deco, task_deco
+from vortex.data.geometries import hgeometry_deco
 from vortex.syntax.stdattrs import model_deco, cutoff_deco
+from promethee.syntax.stdattrs import promid_deco, version_deco, task_deco
+
 
 #: No automatic export
 __all__ = []
@@ -68,6 +70,51 @@ class PrometheeMask(StaticResource):
     @property
     def realkind(self):
         return "mask"
+
+
+class PrometheeGeoMask(PrometheeMask):
+    """
+    Static netcdf file containing raster representations of all the geographical
+    areas covered by a promethee production, on multiple grids.
+
+    Then, a PrometheeMask is like a dataset of ndarrays. Each ndarray is the
+    representation (a binary matrix) on a given geometry (e.g. 'EURW1S100') and
+    has the following dimensions :
+
+    * latitude
+    * longitude
+    * id (i.e. geographical zone identifier, e.g. "Haute-Garonne").
+
+    A mask is a static resource because it can be used ad vitam eternam, as long
+    as its configuration does not change. That is the reason why a given mask (
+    identified by its 'promid' attribute) is versioned : if its configuration changes,
+    the version also changes, thus the Resource itself changes.
+
+    Inheritance:
+
+    * :class:`vortex.data.outflow.StaticGeoResource`
+
+    Attrs:
+
+    * kind (str) : The resource's kind. Among 'promethee_mask' or 'mask'.
+    * promid (str) : The Promethee production identifier.
+    * version (str) : The version of the mask used. Usually it is the MD5 sum of
+      the resource's configuration.
+    * nativefmt (str) : The resource's storage format. Must be 'netcdf'.
+
+    """
+
+    _footprint = [
+        hgeometry_deco,
+        dict(
+            info = "Promethee mask (with geometry)",
+            attr = dict(
+                kind    = dict(
+                    values      = ["promethee_geomask", "geomask"]
+                )
+            )
+        )
+    ]
 
 
 class PrometheeNoDateBdpeResource(Resource):
