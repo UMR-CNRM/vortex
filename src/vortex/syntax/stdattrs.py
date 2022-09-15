650216b978ad436b4ba6379079e08efb01b054a1
Pascal Lamboley <pascal.lamboley@meteo.fr>
2022-09-15 15:02:10 +0200

    Allow key 'aromeom_common' for a binary

--- src/vortex/syntax/stdattrs.py
+++ src/vortex/syntax/stdattrs.py
@@ -38,7 +38,7 @@ models = set([
 ])
 
 #: Possible values for the most common binaries.
-binaries = set(['arpege', 'aladin', 'arome', 'batodb', 'peace', 'mocage', 'sumo',
+binaries = set(['arpege', 'aladin', 'arome', 'aromeom_common', 'batodb', 'peace', 'mocage', 'sumo',
                 'corromegasurf', 'mesonh', 'safran', 'surfex', 'macc', 'mktopbd',
                 'ifs', 'oops', 'assistance', 'arpifs', 'mfwam', 'mfwam_interp',
                 'ww3', 'ww3_prnc', 'ww3_bound', 'ww3_ncgrb', 'ial', 'alaro',
