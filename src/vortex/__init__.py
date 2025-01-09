50033f2cc4018b517242102ca092efe2e37fed44
tlestang <thibault.lestang@meteo.fr>
2025-01-09 11:05:19 +0100

    Bump version to beta

--- src/vortex/__init__.py
+++ src/vortex/__init__.py
@@ -20,10 +20,10 @@ of the very high level interface defined in the :mod:`vortex.toolbox` module is
 strongly advised.
 """
 
-__version__ = '2.0.0a1'
+__version__ = '2.0.0b1'
 __prompt__ = 'Vortex v-' + __version__ + ':'
 
-__nextversion__ = '2.0.0a2'
+__nextversion__ = '2.0.0b2'
 __tocinfoline__ = 'VORTEX core package'
 
 __all__ = [
