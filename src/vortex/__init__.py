62e90ba12ce462e9032bf1e340f5a487599a4a4e
tlestang <thibault.lestang@meteo.fr>
2025-07-31 15:51:17 +0200

    Release 2.1.2

--- src/vortex/__init__.py
+++ src/vortex/__init__.py
@@ -53,10 +53,10 @@ from .toolbox import algo as task
 
 from . import nwp as nwp  # footprints import
 
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 __prompt__ = "Vortex v-" + __version__ + ":"
 
-__nextversion__ = "2.1.1"
+__nextversion__ = "2.1.3"
 __tocinfoline__ = "VORTEX core package"
 
 __all__ = [
