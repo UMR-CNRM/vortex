9caccc94cb7fb0ba5a6430ce2ba60ffcaa3e3915
tlestang <thibault.lestang@meteo.fr>
2025-09-26 16:30:52 +0200

    Fix expected value for file path in test

--- tests/test_user_remote_dtree.py
+++ tests/test_user_remote_dtree.py
@@ -82,7 +82,7 @@ def test_oper_tree(mocked_glove, mocked_smartftget):
     rh = vtx.input(**fp)[0]
     rh.get()
     mocked_smartftget.assert_called_once_with(
-        "/path/to/op/tree/arpege/4dvarfr/oper/2025/06/16/T0000P/forecast/historic.arpege.tl1798-c22+0002:00.fa",
+        "/path/to/op/tree/arpege/4dvarfr/OPER/2025/06/16/T0000P/forecast/historic.arpege.tl1798-c22+0002:00.fa",
         "local_file_name",
         hostname="example.meteo.fr",
         logname=None,
