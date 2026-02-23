cfe624825866172564f7bf22e4bc51971e86cc00
tlestang <thibault.lestang@meteo.fr>
2026-02-23 15:39:31 +0100

    Release 2.3.1

--- src/vortex/__init__.py
+++ src/vortex/__init__.py
@@ -54,10 +54,10 @@ from .toolbox import VortexForceComplete as VortexForceComplete
 
 from . import nwp as nwp  # footprints import
 
-__version__ = "2.3.0"
+__version__ = "2.3.1"
 __prompt__ = "Vortex v-" + __version__ + ":"
 
-__nextversion__ = "2.3.1"
+__nextversion__ = "2.3.2"
 __tocinfoline__ = "VORTEX core package"
 
 __all__ = [
