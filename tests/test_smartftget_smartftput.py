f328cc7b2c863ce85e83f071dc36aac5286e240d
nicolasCtd <nicolas.coutand@meteo.fr>
2026-06-30 10:39:21 +0200

    Update test_smartftget_smartftput.py

--- tests/test_smartftget_smartftput.py
+++ tests/test_smartftget_smartftput.py
@@ -1,53 +1,54 @@
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 from vortex.tools.systems import Linux34p
-from vortex.config import set_config
 from vortex.tools.net import DEFAULT_FTP_PORT
-
-import ftserv
-
 
 SOURCE = "/path/to/data"
 DESTINATION = "/path/to/destination"
 HOSTNAME = "hendrix.meteo.fr"
 LOGNAME = "username"
-FTSERV_PATTERNS = "(belenos|taranis|sxcoope)transfert\\d.\\1hpc.meteo.fr|sxcoope1"
 
-def make_system():
+
+def true_getcond(cpipeline=None):
+    return True
+
+
+def false_getcond(cpipeline=None):
+    return False
+
+
+def true_putcond(cpipeline=None):
+    return True
+
+
+def false_putcond(cpipeline=None):
+    return False
+
+
+def add_new_method(getfunc, putfunc, getcond, putcond):
     system = Linux34p()
     system.register_ftp_method(
-            getfunc=ftserv.ftserv.rawftget,
-            putfunc=ftserv.ftserv.rawftput,
-            getcond=ftserv.ftserv._use_ftserv_get,
-            putcond=ftserv.ftserv._use_ftserv_put,
-        )
+        getfunc=getfunc,
+        putfunc=putfunc,
+        getcond=getcond,
+        putcond=putcond,
+    )
     return system
 
-@patch("ftserv.ftserv.rawftget")
+
+# smartftget -> default method.
 @patch("vortex.tools.systems.OSExtended.ftget")
-def test_smartftget_uses_ftget_when_ftserv_condition_is_false(mocked_ftget, mocked_rawftget):
-    # VORTEX_CONFIG["ftserv"]["hostname_patterns"] is empty.
-    # One expects:
-    #   - FtServ condition is False;
-    #   - rawftget is NOT called;
-    #   - ftget is called once.
-    
-    set_config("ftserv", 
-               "hostname_patterns", 
-               [],
-               )
-    
-    system = make_system()
-    
-    system.smartftget(SOURCE, 
-                      DESTINATION, 
-                      hostname=HOSTNAME, 
-                      logname=LOGNAME,
-                      port=DEFAULT_FTP_PORT,
-                      )
-    
-    mocked_rawftget.assert_not_called()
-    
+def test_smartftget(mocked_ftget):
+    system = Linux34p()
+
+    system.smartftget(
+        SOURCE,
+        DESTINATION,
+        hostname=HOSTNAME,
+        logname=LOGNAME,
+        port=DEFAULT_FTP_PORT,
+    )
+
     mocked_ftget.assert_called_once_with(
         SOURCE,
         DESTINATION,
@@ -55,68 +56,22 @@ def test_smartftget_uses_ftget_when_ftserv_condition_is_false(mocked_ftget, mock
         logname=LOGNAME,
         port=DEFAULT_FTP_PORT,
         fmt=None,
-        )
+    )
 
-@patch("ftserv.ftserv.rawftget")
-@patch("vortex.tools.systems.OSExtended.ftget")
-def test_smartftget_uses_rawftget_when_ftserv_condition_is_true(mocked_ftget, mocked_rawftget):   
-    # VORTEX_CONFIG["ftserv"]["hostname_patterns"] contains the list of the
-    # authorized machines where FtServ may be used.
-    # One expects:
-    #   - FtServ condition is True;
-    #   - ftget is NOT called;
-    #   - rawftget is called once.
-    
-    system = make_system()
-    set_config("ftserv", 
-               "hostname_patterns", 
-               [FTSERV_PATTERNS],
-               )
-    
-    system.smartftget(SOURCE, 
-                      DESTINATION, 
-                      hostname=HOSTNAME, 
-                      logname=LOGNAME,
-                      port=DEFAULT_FTP_PORT,
-                      )
-    
-    mocked_ftget.assert_not_called()
-    
-    mocked_rawftget.assert_called_once_with(
+
+# smartftput -> default method.
+@patch("vortex.tools.systems.OSExtended.ftput")
+def test_smartftput(mocked_ftput):
+    system = Linux34p()
+
+    system.smartftput(
         SOURCE,
         DESTINATION,
         hostname=HOSTNAME,
         logname=LOGNAME,
         port=DEFAULT_FTP_PORT,
-        fmt=None,
-        )
+    )
 
-
-@patch("ftserv.ftserv.rawftput")
-@patch("vortex.tools.systems.OSExtended.ftput")
-def test_smartftput_uses_ftput_when_ftserv_condition_is_false(mocked_ftput, mocked_rawftput):
-    # VORTEX_CONFIG["ftserv"]["hostname_patterns"] is empty.
-    # One expects:
-    #   - FtServ condition is False;
-    #   - rawftput is NOT called
-    #   - ftput is called once.
-    
-    set_config("ftserv", 
-               "hostname_patterns", 
-               [],
-               )
-
-    system = make_system()
-    
-    system.smartftput(SOURCE, 
-                      DESTINATION, 
-                      hostname=HOSTNAME, 
-                      logname=LOGNAME,
-                      port=DEFAULT_FTP_PORT,
-                      )   
-
-    mocked_rawftput.assert_not_called()
-    
     mocked_ftput.assert_called_once_with(
         SOURCE,
         DESTINATION,
@@ -124,37 +79,61 @@ def test_smartftput_uses_ftput_when_ftserv_condition_is_false(mocked_ftput, mock
         logname=LOGNAME,
         port=DEFAULT_FTP_PORT,
         cpipeline=None,
-        sync=False,
         fmt=None,
-        )
+        sync=False,
+    )
 
 
-@patch("ftserv.ftserv.rawftput")
+# smartftget -> new method if getcond=True.
+@patch("vortex.tools.systems.OSExtended.ftget")
+def test_smartftget_uses_new_method_when_getcond_is_true(mocked_ftget):
+    mocked_rawftget = Mock()
+    mocked_rawftput = Mock()
+
+    system = add_new_method(
+        mocked_rawftget, mocked_rawftput, true_getcond, true_putcond
+    )
+
+    system.smartftget(
+        SOURCE,
+        DESTINATION,
+        hostname=HOSTNAME,
+        logname=LOGNAME,
+        port=DEFAULT_FTP_PORT,
+    )
+
+    mocked_ftget.assert_not_called()
+
+    mocked_rawftget.assert_called_once_with(
+        SOURCE,
+        DESTINATION,
+        hostname=HOSTNAME,
+        logname=LOGNAME,
+        port=DEFAULT_FTP_PORT,
+        fmt=None,
+    )
+
+
+# smartftput -> new method if putcond=True.
 @patch("vortex.tools.systems.OSExtended.ftput")
-def test_smartftput_uses_rawftput_when_ftserv_condition_is_true(mocked_ftput, mocked_rawftput):
-    # VORTEX_CONFIG["ftserv"]["hostname_patterns"] contains the list of the
-    # authorized machines where FtServ may be used.
-    # One expects:
-    #   - FtServ condition is True;
-    #   - ftput is NOT called;
-    #   - rawftput is called once.
-    
-    set_config("ftserv", 
-                "hostname_patterns", 
-                [FTSERV_PATTERNS],
-                )
+def test_smartftput_uses_new_method_when_putcond_is_true(mocked_ftput):
+    mocked_rawftget = Mock()
+    mocked_rawftput = Mock()
 
-    system = make_system()
-    
-    system.smartftput(SOURCE, 
-                      DESTINATION, 
-                      hostname=HOSTNAME, 
-                      logname=LOGNAME,
-                      port=DEFAULT_FTP_PORT,
-                      )
-    
+    system = add_new_method(
+        mocked_rawftget, mocked_rawftput, true_getcond, true_putcond
+    )
+
+    system.smartftput(
+        SOURCE,
+        DESTINATION,
+        hostname=HOSTNAME,
+        logname=LOGNAME,
+        port=DEFAULT_FTP_PORT,
+    )
+
     mocked_ftput.assert_not_called()
-    
+
     mocked_rawftput.assert_called_once_with(
         SOURCE,
         DESTINATION,
@@ -164,4 +143,66 @@ def test_smartftput_uses_rawftput_when_ftserv_condition_is_true(mocked_ftput, mo
         fmt=None,
         cpipeline=None,
         sync=False,
-        )
+    )
+
+
+# smartftget -> fallback to the default method if getcond=False.
+@patch("vortex.tools.systems.OSExtended.ftget")
+def test_smartftget_uses_default_method_when_getcond_is_false(mocked_ftget):
+    mocked_rawftget = Mock()
+    mocked_rawftput = Mock()
+
+    system = add_new_method(
+        mocked_rawftget, mocked_rawftput, false_getcond, false_putcond
+    )
+
+    system.smartftget(
+        SOURCE,
+        DESTINATION,
+        hostname=HOSTNAME,
+        logname=LOGNAME,
+        port=DEFAULT_FTP_PORT,
+    )
+
+    mocked_rawftget.assert_not_called()
+
+    mocked_ftget.assert_called_once_with(
+        SOURCE,
+        DESTINATION,
+        hostname=HOSTNAME,
+        logname=LOGNAME,
+        port=DEFAULT_FTP_PORT,
+        fmt=None,
+    )
+
+
+# smartftput -> fallback to the default method if putcond=False.
+@patch("vortex.tools.systems.OSExtended.ftput")
+def test_smartftput_uses_default_method_when_putcond_is_false(mocked_ftput):
+    mocked_rawftget = Mock()
+    mocked_rawftput = Mock()
+
+    system = add_new_method(
+        mocked_rawftget, mocked_rawftput, false_getcond, false_putcond
+    )
+
+    system.smartftput(
+        SOURCE,
+        DESTINATION,
+        hostname=HOSTNAME,
+        logname=LOGNAME,
+        port=DEFAULT_FTP_PORT,
+    )
+
+    mocked_rawftput.assert_not_called()
+
+    mocked_ftput.assert_called_once_with(
+        SOURCE,
+        DESTINATION,
+        hostname=HOSTNAME,
+        logname=LOGNAME,
+        port=DEFAULT_FTP_PORT,
+        fmt=None,
+        cpipeline=None,
+        sync=False,
+    )
